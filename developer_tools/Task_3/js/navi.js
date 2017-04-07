1275bc91c0a567c5785e9fe1aa67b20f6bb72f45
Lisa Bogdanova <ukloikup@yandex.ru>
2017-04-07 14:26:38 +0400

    navi.js

--- developer_tools/Task_3/js/navi.js
+++ developer_tools/Task_3/js/navi.js
@@ -1,22 +1,21 @@
-window.onload = function() {
-var browserservice = document.getElementById('browser');
+window.onload = function () {
+    var browserservice = document.getElementById('browser');
 
-if (window.navigator.userAgent.search(/Chrome/) != -1) {
-	browserservice.innerHTML = '<p>' + navigator.userAgent + '</p>'+ '<br><b>' + navigator.appVersion + '</b>';
+    if (window.navigator.userAgent.search(/Chrome/) != -1) {
+        browserservice.innerHTML = '<p>' + navigator.userAgent + '</p>' + '<br><b>' + navigator.appVersion + '</b>';
+    }
+    else if (window.navigator.userAgent.search(/Firefox/) != -1) {
+        browserservice.innerHTML = '<p>' + navigator.appName + '</p>' + '<br><b>' + navigator.cookieEnabled + '</b>';
+    }
+    else if (window.navigator.userAgent.search(/Opera/) != -1) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+        function showPosition(position) {
+            x.innerHTML = "Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude;
+        }
+    };
 }
-else if (window.navigator.userAgent.search(/Firefox/) != -1) {
-	browserservice.innerHTML = '<p>' + navigator.appName + '</p>'+ '<br><b>' + navigator.cookieEnabled + '</b>';
-}
-
-
-
-else if (window.navigator.userAgent.search(/IE/) != -1) {
-
-browserservice.innerHTML = '<p>' + navigator.platform + '</p>';
-	
-var lat = position.coords.latitude;
-	
-var lon = position.coords.longitude;
-	
-browserservice.innerHTML += lat + '<br>' + lon; 
-};
