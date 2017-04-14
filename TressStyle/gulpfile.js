89b4d359602e3e8bc8ae95dce80c1f76c629ab41
Lisa Bogdanova <ukloikup@yandex.ru>
2017-04-14 19:51:37 +0400

    sait homepage server

--- TressStyle/gulpfile.js
+++ TressStyle/gulpfile.js
@@ -1,20 +1,30 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync').create(),
-    runSequence = require('run-sequence');
-// var reload = browserSync.reload;
-gulp.task('default', ['img','watch', 'server']);
+    runSequence = require('run-sequence'),
+    webserver = require('gulp-webserver'),
+    livereload = require('gulp-livereload');
+
+gulp.task('default', ['img', 'sass', 'webserver']);
 
 const params = {
     out: 'dist/',
-    stylesSass: 'app/**/*.sass',
+    stylesSass: 'app/sass/*.sass'
 };
+
+gulp.task('webserver', function() {
+    gulp.src('dist')
+        .pipe(webserver({
+            livereload: true,
+            host: '0.0.0.0',
+            open: true
+        }));
+});
 
 gulp.task('sass', function() {
     return gulp.src(params['stylesSass'])
         .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.stream());
 });
 
 gulp.task('img', function() {
@@ -22,24 +32,24 @@ gulp.task('img', function() {
         .pipe(gulp.dest('dist/img'))
 });
 
-gulp.task('server', function() {
-    browserSync.init({
-        notify: false,
-        server: {
-            host: '0.0.0.0',
-            baseDir: "./dist",
-        }
-    });
-
-    browserSync.watch(path.join(params['out'], '**', '*.*')).on(
-        'change',
-        browserSync.reload
-    );
-});
-
 gulp.task('watch', function() {
     gulp.watch(['sass/**/*.sass', 'sass/**/*.scss'], ['sass']);
 });
+
+// gulp.task('server', function() {
+//     browserSync.init({
+//         notify: false,
+//         server: {
+//             host: '0.0.0.0',
+//             baseDir: "./dist",
+//         }
+//     });
+
+//     browserSync.watch(path.join(params['out'], '**', '*.*')).on(
+//         'change',
+//         browserSync.reload
+//     );
+// });
 
 // gulp.task('watch', ['sass'], function() {
 
